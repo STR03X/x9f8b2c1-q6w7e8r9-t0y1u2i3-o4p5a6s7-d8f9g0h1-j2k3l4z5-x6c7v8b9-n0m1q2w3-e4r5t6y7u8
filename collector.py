64263ce82868a19aea133c14f30fc50b85b82adb
STR03X <stroex@proton.me>
2026-07-18 18:24:29 +0300

    Update collector.py

--- collector.py
+++ collector.py
@@ -1027,7 +1027,7 @@ async def _launch_browser_session(pw, storage_state):
     global active_page
 
     browser = await pw.chromium.launch(
-        headless=True,
+        headless=False,
         args=[
             "--no-sandbox",
             "--disable-dev-shm-usage",
